a6927066c178ff4c14df59388e481f2c552aa400
Tracy Liu <tracyliu.cs06@nctu.edu.tw>
2018-07-11 02:56:27 +0800

    welcome page

--- src/view_header.js
+++ src/view_header.js
@@ -4,7 +4,6 @@ var user_data = fs.readFileSync('data/user.json', 'utf-8')
 var current_book_id = 0
 var current_book = JSON.parse(user_data).user.books[current_book_id]
 
-var loading_url = current_book.chaps[0].subchaps[0].link
 var color = current_book.color
 
 var active_chap = -1
